225f41c2dc3c5391a8e2e84c4481aedd4fe9447a
milomouse <vincent@fea.st>
2011-03-25 10:30:28 -0400

    archlinux bbs homepage again..

--- luakit/globals.lua
+++ luakit/globals.lua
@@ -1,6 +1,6 @@
 -- Global variables for luakit
 globals = {
-    homepage            = "http://en.wikipedia.org/wiki/Main_Page",
+    homepage            = "https://bbs.archlinux.org/search.php?action=show_new",
     scroll_step         = 40,
     zoom_step           = 0.1,
     max_cmd_history     = 100,

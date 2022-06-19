9f2d02281ab06e287fc5d3cfa638a5b302abb522
milomouse (Vincent Anonymouse) <ixsolaris86@gmail.com>
2022-06-19 12:17:19 -0400

    raise warn temperature.

--- polybar/scripts/gputemp.zsh
+++ polybar/scripts/gputemp.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 gputemp=$(nvidia-smi --format=nounits,csv,noheader --query-gpu=temperature.gpu | xargs echo)
-if [[ "$gputemp" -ge 60 && "$gputemp" -lt 80 ]] ; then
+if [[ "$gputemp" -ge 65 && "$gputemp" -lt 80 ]] ; then
   echo "  %{F#EAE900}%{F-} $gputemp°C"
 elif [[ "$gputemp" -ge 80 ]] ; then
   echo "  %{F#C2003B}%{F-} $gputemp°C"

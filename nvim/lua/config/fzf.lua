46f7e9d491dfe59b4a2215a11b5b0cc0b0e5ee83
milomouse (Vincent) <ixsolaris86@gmail.com>
2025-01-31 06:59:02 -0500

    clean-up; change a few bindings

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -2,29 +2,40 @@
 
 -- colors
 vim.g.fzf_colors = {
-  fg =      {'fg', 'LineNr'},
-  bg =      {'bg', 'Normal'},
-  hl =      {'fg', 'Normal'},
-  info =    {'fg', 'LineNr'},
-  border =  {'fg', 'Comment'},
-  prompt =  {'fg', 'Conditional'},
-  pointer = {'fg', 'Conditional'},
-  marker =  {'fg', 'Error'},
-  spinner = {'fg', 'Label'},
-  header =  {'fg', 'Comment'},
+    fg =        {'fg', 'Comment'},
+    bg =        {'bg', 'Normal'},
+    hl =        {'fg', 'Identifier'},
+    info =      {'fg', 'Number'},
+    border =    {'fg', 'Comment'},
+    prompt =    {'fg', 'Identifier'},
+    pointer =   {'fg', 'Question'},
+    marker =    {'fg', 'Error'},
+    spinner =   {'fg', 'Label'},
+    header =    {'fg', 'Comment'},
 }
 
--- keybindings
-vim.keymap.set('n', '<C-i>', ':FZF $H<CR>', opts)
-vim.keymap.set('n', '<C-o>', ':FZF<CR>', opts)
-vim.keymap.set('n', '<C-p>', ':FZF ..<CR>', opts)
-vim.keymap.set('n', '<C-f>', ':History<CR>', opts)
-vim.keymap.set('n', '<C-s>', ':BLines<CR>', opts)
-vim.keymap.set('n', '<C-b>', ':Buffers<CR>', opts)
-vim.keymap.set('n', '<leader>ge', ':GFiles<CR>', opts)
+
+-- keybindings: general
+vim.keymap.set('n', '<leader>ab', ':Buffers<CR>', opts)-- access open buffers
+vim.keymap.set('n', '<leader>ac', ':FZF<CR>', opts)-- access current directory
+vim.keymap.set('n', '<leader>ap', ':FZF ..<CR>', opts)-- access parent directory
+vim.keymap.set('n', '<leader>ah', ':FZF $H<CR>', opts)-- access user base directory
+vim.keymap.set('n', '<leader>au', ':FZF $HOME<CR>', opts)-- access user config directory
+
+-- keybindings: search/find/grep
+vim.keymap.set('n', '<leader>fd', ':Rg<CR>', opts)-- grep within directory
+vim.keymap.set('n', '<leader>fa', ':Lines<CR>', opts)-- grep within buffers
+vim.keymap.set('n', '<leader>fb', ':BLines<CR>', opts)-- grep within buffer
+vim.keymap.set('n', '<leader>fh', ':History<CR>', opts)-- show file history
+vim.keymap.set('n', '<leader>fc', ':History:<CR>', opts)-- show cmd history
+
+-- keybindings: git
+vim.keymap.set('n', '<leader>gf', ':GFiles<CR>', opts)-- show git files
+
+-- keybindings: menu
 vim.cmd([[
-  let g:fzf_action = {
-    \ 'ctrl-t': 'tab split',
+    let g:fzf_action = {
     \ 'ctrl-h': 'split',
-    \ 'ctrl-v': 'vsplit' }
+    \ 'ctrl-v': 'vsplit',
+    \ 'ctrl-n': 'tab split' }
 ]])

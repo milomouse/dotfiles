9e8244361b7423d2dc9c75b21e9d53ca4e648203
milomouse (Vincent) <ixsolaris86@gmail.com>
2025-01-31 10:17:32 -0500

    initial commit for lspkind configuration

---
+++ nvim/lua/config/lspkind.lua
@@ -0,0 +1,44 @@
+-- file: ${XDG_CONFIG_HOME}/nvim/lua/config/lspkind.lua
+
+-- initialize lspkind:
+
+-- setup() is also available as an alias
+require('lspkind').init({
+    -- defines how annotations are shown
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+    mode = 'symbol_text',
+
+    -- default symbol map
+    -- can be either 'default' (requires nerd-fonts font) or
+    -- 'codicons' for codicon preset (requires vscode-codicons font)
+    preset = 'codicons',
+
+    -- override preset symbols
+    symbol_map = {
+      Text = "󰉿",
+      Method = "󰆧",
+      Function = "󰊕",
+      Constructor = "",
+      Field = "󰜢",
+      Variable = "󰀫",
+      Class = "󰠱",
+      Interface = "",
+      Module = "",
+      Property = "󰜢",
+      Unit = "󰑭",
+      Value = "󰎠",
+      Enum = "",
+      Keyword = "󰌋",
+      Snippet = "",
+      Color = "󰏘",
+      File = "󰈙",
+      Reference = "󰈇",
+      Folder = "󰉋",
+      EnumMember = "",
+      Constant = "󰏿",
+      Struct = "󰙅",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "",
+    },
+})
